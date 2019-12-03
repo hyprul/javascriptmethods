68f331f3e15e51b2c96a20e2b472e56595759436
Sean Huang <hyprul@ucla.edu>
2019-12-02 19:54:57 -0600

    substring

--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -1,41 +1,49 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
-function double(x) {
-    return 2 * x;
-}
+// //Map -Create a new array by doing something with each item in an array.
+// function double(x) {
+//     return 2 * x;
+// }
 
-const newNumbers = numbers.map(double);
+// const newNumbers = numbers.map(double);
 
-//Filter - Create a new array by keeping the items that return true.
+// //Filter - Create a new array by keeping the items that return true.
 
 
-const newNumbers = numbers.filter(function (number) {
-    return number < 10;
-});
+// const newNumbers = numbers.filter(function (number) {
+//     return number < 10;
+// });
 
-var newNumbers = [];
+// var newNumbers = [];
 
-numbers.forEach(function (num) {
-    if (num < 10) {
-        newNumbers.push(num);
-    }
+// numbers.forEach(function (num) {
+//     if (num < 10) {
+//         newNumbers.push(num);
+//     }
+// })
+
+// //Reduce - Accumulate a value by doing something to each item in an array.
+
+// var newNumber = numbers.reduce(function (acculumator, currentNumber) {
+//     return acculumator + currentNumber;
+// })
+
+// //Find - find the first item that matches from an array.
+
+// const newNumber = numbers.find(function (num) {
+//     return num > 10;
+// })
+
+// //FindIndex - find the index of the first item that matches.
+
+// const index = numbers.findIndex(function (num) {
+//     return num > 10;
+// })
+
+import emojipedia from './emojipedia';
+
+const meanings = emojipedia.map(function (emoji) {
+    return emoji.meaning.substr(0, 99);
 })
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-
-var newNumber = numbers.reduce(function (acculumator, currentNumber) {
-    return acculumator + currentNumber;
-})
-
-//Find - find the first item that matches from an array.
-
-const newNumber = numbers.find(function (num) {
-    return num > 10;
-})
-
-//FindIndex - find the index of the first item that matches.
-
-const index = numbers.findIndex(function (num) {
-    return num > 10;
-})
+console.log(meanings);
