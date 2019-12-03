3dda203b9ebba111393b0277fbe8c8c392b84f90
Sean Huang <hyprul@ucla.edu>
2019-12-02 19:41:53 -0600

    methods

--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -1,10 +1,22 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+function double(x) {
+    return 2 * x;
+}
+
+const newNumbers = numbers.map(double);
 
 //Filter - Create a new array by keeping the items that return true.
 
+
+const newNumbers = numbers.filter(function (number) {
+    return number < 10;
+});
+
+
 //Reduce - Accumulate a value by doing something to each item in an array.
+
 
 //Find - find the first item that matches from an array.
 
